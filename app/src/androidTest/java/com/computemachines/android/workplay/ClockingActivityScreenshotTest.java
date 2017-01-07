8d6fa5400cfd336781e76f085ace4c3c9d84cbc1
Satyajit Malugu <smalugu@godaddy.com>
2017-01-07 11:43:26 -0800

    didn't work after removing largetest annotation

--- app/src/androidTest/java/com/computemachines/android/workplay/ClockingActivityScreenshotTest.java
+++ app/src/androidTest/java/com/computemachines/android/workplay/ClockingActivityScreenshotTest.java
@@ -2,7 +2,6 @@ package com.computemachines.android.workplay;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import com.google.android.libraries.cloudtesting.screenshots.ScreenShotter;
 
@@ -11,14 +10,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 
-/**
- * Created by tparker on 5/9/16.
- */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class ClockingActivityScreenshotTest {
 
     @Rule
@@ -26,9 +20,7 @@ public class ClockingActivityScreenshotTest {
 
     @Test
     public void takeScreenshot() {
-        ScreenShotter.takeScreenshot("main_screen_1", activityRule.getActivity());
-        onView(withId(R.id.play_label)).perform(click());
-
-        ScreenShotter.takeScreenshot("main_screen_2", activityRule.getActivity());
+        onView(isRoot());
+        ScreenShotter.takeScreenshot("main_screen_3", activityRule.getActivity());
     }
 }
